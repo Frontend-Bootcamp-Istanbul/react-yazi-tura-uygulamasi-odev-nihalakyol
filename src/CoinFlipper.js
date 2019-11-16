982b24682e6016d14b38628c681108e4b58afca9
Nihal Akyol <nihal.akyoll@gmail.com>
2019-11-16 09:37:11 +0300

    Ödevin Yüklenmesi.

--- src/CoinFlipper.js
+++ src/CoinFlipper.js
@@ -9,15 +9,35 @@ class CoinFlipper extends Component {
     // Daha sonra şu anda paranın dönüp dönmeme durumunu da veriyoruz, başlangıçta para atılmamış olduğundan "false" olarak verdik.
     this.state = {
         side: "tura",
-        donuyor: false
+        donuyor: false,
+        atisSayisi : 0,
+        atisTura: 0,
+        atisYazi:0
     }
   }
   handleClick = () => {
     // "At!" butonuna tıkladığımızda paranın dönmesini istiyoruz, bu yüzden "donuyor" durumunu "true" yapıyoruz.
     this.setState({donuyor: true});
     // 1 saniye kadar dönmesi yeterli, bu yüzden 1 saniye sonra "donuyor" durmunu tekrar "false" yapıyoruz.
-    setTimeout(() => this.setState({donuyor: false}), 1000);
-  };
+    setTimeout(() => this.setState({donuyor: false}), 500);
+    // Butona basıldıkça "side" props'umuzu bir random değişken vasıtsıyla değiştiriyoruz.
+    if (Math.random()>0.5) {
+      this.setState({side: "yazi"});
+      this.setState({atisYazi: this.state.atisYazi+1});
+    } else {
+      this.setState({side:"tura",
+                    atisTura: this.state.atisTura+1})
+    }
+    // Butona basıldıkça "atisSayisi" props'umuzu bir artırıyoruz.
+    //this.state.atisSayisi+=1 => bu yanlış.
+    this.setState({atisSayisi:this.state.atisSayisi+1})
+    // atisSayisi 5 olduğunda sıfırlanır.
+    if (this.state.atisSayisi === 5) {
+      this.setState({atisSayisi:0,
+      atisTura:0,
+      atisYazi:0})
+    }
+	};
 
   render(){
     return (
@@ -25,16 +45,17 @@ class CoinFlipper extends Component {
         <h1>Yazı mı Tura mı?</h1>
         <Coin side={this.state.side} donuyor={this.state.donuyor} />
         <button onClick={this.handleClick} >At!</button>
-        <p>
+        <p className="text-primary">
+
             Toplam
-            <strong> 5 </strong>
+            <strong> {this.state.atisSayisi} </strong>
             atıştan
-            <strong> 3 </strong>
-            ü tura
-            <strong> 2 </strong>
-            si yazı geldi.</p>
+            <strong> {this.state.atisTura} </strong>
+             tura
+            <strong> {this.state.atisYazi} </strong>
+             yazı geldi.</p>
       </div>
-    )
+	  )
   }
 }
 
